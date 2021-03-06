dc8efc87fbf3abdaec20d795adf086109e3ba527
Janine <janine.duhre@googlemail.com>
2021-03-06 16:05:57 +0100

    tutorial try outs

--- src/script.js
+++ src/script.js
@@ -7,12 +7,45 @@
 //  1c: clear input value: inputValue = ''
 //  1d: prevent form to submit by default: event.preventDefault();
 
+//const inputValue = document.querySelector('#textInput').value;
+//form.addEventListener('submit', addNewTodo)
+
 
 // 2_ Add new todo to ul
 // 2a: create addNewTodo function: const addNewTodo = function() { ... }
 // 2b: grab ul element: const listEl = getElementbyId('myTasks)
 // 2c: create new todo template: const template = `<li class="todo-item">${inputValue}</li>`
 // 3c: add template to ul: listEl.innerHTML += template
-const addNewTodo = function () {
+//const addNewTodo = function () {
   // TODO: Add new todo
-};
+//};
+
+
+// 2_with instructions from https://medium.com/@suryashakti1999/to-do-list-app-using-javascript-for-absolute-beginners-13ea9e38a033
+const todoInput = document.querySelector('.todo_input');
+const todoButton = document.querySelector('.todo_button');
+const todoList = document.querySelector('.todo_list');
+todoButton.addEventListener("click", addTodo)
+
+function addTodo(event) {
+    event.preventDefault();
+    //todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //todo LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo_item');
+    todoDiv.appendChild(newTodo);
+    if(todoInput.value === ""){
+        return null
+    }
+
+// 3_with instructions from https://www.youtube.com/watch?v=8ogdhvxShe0&ab_channel=CodifyAcademy
+//function todoList() {
+    //var item = document.getElementById("todoInput").inputValue;
+    //var text = document.createTextNode(item);
+    //var newItem = document.createElement("li");
+    //newItem.appendChild(text);
+    //document.getElementById("todoList").appendchild(newItem);
+//}
